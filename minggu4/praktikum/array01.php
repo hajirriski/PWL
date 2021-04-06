eea5b617a254a85cef78e21db6318d0d14693017
hajirriski <72801049+hajirriski@users.noreply.github.com>
2021-04-06 13:29:22 +0700

    Update array01.php

--- minggu4/praktikum/array01.php
+++ minggu4/praktikum/array01.php
@@ -1,1 +1,12 @@
-a
+<?php
+$arrBuah = array ("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrBuah[0]; //Mangga
+echo $arrBuah[3]; //Jeruk
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+echo $arrWarna[0]; //Merah
+echo $arrWarna[2]; //Hijau
+?>
